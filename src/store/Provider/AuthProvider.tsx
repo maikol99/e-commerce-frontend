e49b5129f8a4fd8b7c497cfeb6caa0fd8d9e141d
Maikol99 <alegremaikol21@gmail.com>
2025-11-27 19:49:04 -0300

    add Provider

---
+++ src/store/Provider/AuthProvider.tsx
@@ -0,0 +1,47 @@
+import React, { useEffect } from "react";
+import {useVerifyAuthMutation} from "../api"
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { RootState } from "../store";
+import { logout, setEmailVerified, setUser } from "../slice/userSlice";
+import BookLoader from "@/lib/BookLoader";
+
+export default function AuthCheck({children} : {children:React.ReactNode}) {
+    const [verifyAuth, {isLoading}] = useVerifyAuthMutation();
+    const [isCheckingAuth,setIsCheckingAuth] = useState(true);
+    const dispatch = useDispatch();
+    const user = useSelector((state: RootState) => state.user.user)
+    const isLoggedIn = useSelector((state: RootState) => state.user.isLoggedIn)
+
+    useEffect(() => {
+        const checkAuth = async() => {
+            try {
+                const response = await verifyAuth({}).unwrap();
+                if(response.success){
+                    dispatch(setUser(response.data));
+                    dispatch(setEmailVerified(response.data.isEmailVerified))
+                }else{
+                    dispatch(logout())
+                }
+            } catch (error) {
+                dispatch(logout())
+            }
+            finally{
+                setIsCheckingAuth(false);
+            }
+        };
+
+        if(!user && isLoggedIn){
+            checkAuth();
+        }else{
+            setIsCheckingAuth(false);
+        }
+    },[verifyAuth,dispatch,user])
+
+    if(isLoading || isCheckingAuth){
+        return <BookLoader/>
+    }
+    
+    return <>{children}</>
+
+}
